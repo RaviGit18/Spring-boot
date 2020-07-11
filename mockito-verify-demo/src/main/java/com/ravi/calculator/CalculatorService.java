17ce34bb04dacd8a7a998f0374658ba27d3f2006
Ravi Ranjan <r25july@gmail.com>
2020-07-11 17:14:48 +0530

    mockito verify demo

---
+++ mockito-verify-demo/src/main/java/com/ravi/calculator/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.ravi.calculator;
+
+public interface CalculatorService {
+	public double add(double input1, double input2);
+
+	public double subtract(double input1, double input2);
+
+	public double multiply(double input1, double input2);
+
+	public double divide(double input1, double input2);
+}
---
+++ mockito-verify-demo/src/test/java/com/ravi/calculator/MathApplicationTester.java
@@ -0,0 +1,41 @@
+package com.ravi.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+//@RunWith attaches a runner with the test class to initialize the test data
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester {
+
+//@InjectMocks annotation is used to create and inject the mock object
+	@InjectMocks
+	MathApplication mathApplication = new MathApplication();
+
+//@Mock annotation is used to create the mock object to be injected
+	@Mock
+	CalculatorService calcService;
+
+	@Test
+	public void testAdd() {
+		// add the behavior of calc service to add two numbers
+		when(calcService.add(10.0, 20.0)).thenReturn(30.00);
+
+		// test the add functionality
+		Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+		
+		//for verify functionality
+		
+		//test the add functionality
+	      Assert.assertEquals(calcService.add(10.0, 20.0),30.0,0);
+	      
+		//verify the behavior
+	     verify(calcService).add(10.0, 20.0);
+	}
+}
